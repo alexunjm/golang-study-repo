eb414e3a5200221fd15c3a49415f9ba709c8e23a
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-15 22:42:05 +0000

    nested structures

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip   int
+}
 type person struct {
 	firstName string
-	lastName string
+	lastName  string
+	contact   contactInfo
 }
 
+/*
 func main() {
-	alex := person{"Alex", "Jaramillo"}
+	alex := person{
+		firstName:"Alex",
+		lastName:"Jaramillo",
+	}
 	fmt.Println(alex)
+} */
+func main() {
+	var alex person
+	fmt.Println(alex)
+	fmt.Printf("%+v\n\n", alex) // plus + to show keys
+
+	alex = person{
+		firstName: "Alex",
+		lastName:  "Jaramillo",
+		contact: contactInfo{
+			email: "",
+		},
+	}
+
+	// fmt.Println(alex)
+	fmt.Printf("%v\n", alex)
+
+	alex.lastName = "Muñoz"
+	alex.contact.email = "contacto@alexanderjaramillo.com"
+	alex.contact.zip = 040062
+	fmt.Printf("%+v\n", alex) // plus + to show keys
 }
