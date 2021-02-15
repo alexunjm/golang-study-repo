dffea71e175c338b85fae31b1a6c871142e50f3f
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-15 17:44:29 +0000

    parse data

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-func main() {
-	cards := newDeck()
+import "fmt"
 
-	cards.print()
+func main() {
+	// cards := newDeck()
+
+	// // cards.print()
+	// hold, change := cards.deal(2)
+
+	// hold.print()
+	// change.print()
+
+	greeting := "Hi there!"
+	// parse of string to byte slice
+	fmt.Println([]byte(greeting))
 }
