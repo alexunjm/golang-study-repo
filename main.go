698aa0e902b4820b4b5005f704c7b7a185372626
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-16 19:04:41 +0000

    use read function to print first 999 chars

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"strings"
+	"net/http"
+	"os"
 )
 
 func main() {
 	/*
-		resp, err := http.Get("http://google.com")
-		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-		fmt.Printf("%+v", resp)
-		ioReadCloser := resp.Body */
-	r := strings.NewReader("Hello, Reader!")
+		r := strings.NewReader("Hello, Reader!")
 
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
+		b := make([]byte, 8)
+		for {
+			n, err := r.Read(b)
+			fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+			fmt.Printf("b[:n] = %q\n", b[:n])
+			if err == io.EOF {
+				break
+			}
 		}
+	*/
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
+	fmt.Printf("%+v", resp)
+
+	bs := make([]byte, 999)
+	resp.Body.Read(bs)
+	fmt.Println("\n\n", string(bs))
 }
