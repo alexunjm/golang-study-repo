f88c08b6373fc68e823d80ed109ad8d87e17430b
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-16 18:54:26 +0000

    how to handle Readers

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hi there!")
+	/*
+		resp, err := http.Get("http://google.com")
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%+v", resp)
+		ioReadCloser := resp.Body */
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
