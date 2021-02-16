70b74392352533ce5225ee461d5afa1d5df4dece
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-16 20:38:10 +0000

    how to use channels

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hi there!")
+	links := []string{
+		"http://google.com",
+		"http://amazon.com",
+		"http://azure.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+		"http://facebook.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkStatus(link, c)
+	}
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
+}
+
+func checkStatus(url string, c chan string) {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(url, "is down")
+		c <- "Might be down I think"
+	}
+
+	fmt.Println(url, "is ok!")
+	c <- "Yep its up"
 }
