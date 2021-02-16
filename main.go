36dcdee0814e588935c7afce8835fb44caa4b7bb
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-16 17:27:29 +0000

    maps

--- main.go
+++ main.go
@@ -2,51 +2,28 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email string
-	zip   int
-}
-type person struct {
-	firstName string
-	lastName  string
-	contactInfo
-}
-
 func main() {
-	var alex person
-	alex.print("empty or default person struct\n")
-
-	alex = person{
-		firstName: "Alex",
-		lastName:  "Jaramillo",
-		contactInfo: contactInfo{
-			email: "contacto@alexanderjaramillo.com",
-		},
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
 	}
-	alex.print("before update without pointer\n")
+	fmt.Println(colors)
 
-	alex.updateNameCheck("Jhon")
-	alex.print("after update without pointer\n")
+	// var otherColors map[string]string
+	otherColors := make(map[string]string)
+	otherColors["white"] = "#ffffff"
+	fmt.Println(otherColors)
 
-	alex.updateName("Jhon")
-	alex.print("after update using pointer\n")
+	otherMap := make(map[int]string)
+	otherMap[10] = "#000010"
+	fmt.Println(otherMap)
 
+	delete(colors, "green")
+	printMap(colors)
 }
 
-func (p person) print(prefix string) {
-	fmt.Printf("%s %+v\n", prefix, p) // plus + to show keys
-}
-
-// creates a p person as a new struct on RAM
-// then, update last created inside func.
-// The source struct expected to update does not change
-func (p person) updateNameCheck(newFirstName string) {
-	p.firstName = newFirstName
-}
-
-// receiver says that p is a pointer to RAM address location,
-// and the value of address is person struct type
-func (p *person) updateName(newFirstName string) {
-	// *p takes the value of RAM address (the person)
-	(*p).firstName = newFirstName
+func printMap(m map[string]string) {
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
