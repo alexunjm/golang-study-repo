31ed6c8f077bd429539a745ae289e72776bcb241
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-16 19:13:34 +0000

    output all response body - use ioReader - use ioWriter - handle wit io.Copy

--- main.go
+++ main.go
@@ -2,32 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	/*
-		r := strings.NewReader("Hello, Reader!")
-
-		b := make([]byte, 8)
-		for {
-			n, err := r.Read(b)
-			fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-			fmt.Printf("b[:n] = %q\n", b[:n])
-			if err == io.EOF {
-				break
-			}
-		}
-	*/
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%+v", resp)
+	// fmt.Printf("%+v", resp)
 
-	bs := make([]byte, 999)
-	resp.Body.Read(bs)
-	fmt.Println("\n\n", string(bs))
+	ioReader := resp.Body
+	ioWriter := os.Stdout
+	io.Copy(ioWriter, ioReader)
 }
