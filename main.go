9d2ed6bca59989257398c03ec75fe2e18daab889
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 15:14:30 +0000

    seasons

--- main.go
+++ main.go
@@ -2,28 +2,39 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
 )
 
 func main() {
-	// time.Now() gets the current time
-	// and in turn, .Hour() gets the current hour from it
-
-	h := time.Now().Hour()
-	fmt.Println("Current hour is", h)
-
-	switch h := time.Now().Hour(); {
-	case h >= 18: // 18 to 23
-		fmt.Println("good evening")
-	case h >= 12: // 12 to 18
-		fmt.Println("good afternoon")
-	case h >= 6: // 6 to 12
-		fmt.Println("good morning")
-	default: // 0 to 5
-		fmt.Println("good night")
+	if len(os.Args) != 2 {
+		fmt.Println("Gimme a month name.\n something like \n$ go run main.go Dec")
+		return
 	}
 
-	// h is not available here
-	// it's bound to switch statement's block
-	// fmt.Println(h)
+	// m := os.Args[1]
+
+	// if m == "Dec" || m == "Jan" || m == "Feb" {
+	// 	fmt.Println("Winter")
+	// } else if m == "Mar" || m == "Apr" || m == "May" {
+	// 	fmt.Println("Spring")
+	// } else if m == "Jun" || m == "Jul" || m == "Aug" {
+	// 	fmt.Println("Summer")
+	// } else if m == "Sep" || m == "Oct" || m == "Nov" {
+	// 	fmt.Println("Fall")
+	// } else {
+	// 	fmt.Println("ERROR: not a month.")
+	// }
+
+	switch m := os.Args[1]; m {
+	case "Dec", "Jan", "Feb":
+		fmt.Println("Winter")
+	case "Mar", "Apr", "May":
+		fmt.Println("Spring")
+	case "Jun", "Jul", "Aug":
+		fmt.Println("Summer")
+	case "Sep", "Oct", "Nov":
+		fmt.Println("Fall")
+	default:
+		fmt.Printf("%q is not a month.\n", m)
+	}
 }
