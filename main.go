d4334f3993249f046d04fe526f61b1d12a70ad48
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 15:29:34 +0000

    for

--- main.go
+++ main.go
@@ -2,36 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Give me a month name")
-		return
+	var sum int
+
+	// sum += 1
+	// sum += 2
+	// sum += 3
+	// sum += 4
+	// sum += 5
+
+	for i := 1; i <= 1000; i++ {
+		sum += i
 	}
 
-	year := time.Now().Year()
-	leap := year%4 == 0 && (year%100 != 0 || year%400 == 0)
-
-	days, month := 28, os.Args[1]
-
-	switch strings.ToLower(month) {
-	case "april", "june", "september", "november":
-		days = 30
-	case "january", "march", "may", "july",
-		"august", "october", "december":
-		days = 31
-	case "february":
-		if leap {
-			days = 29
-		}
-	default:
-		fmt.Printf("%q is not a month.\n", month)
-		return
-	}
-
-	fmt.Printf("%q has %d days.\n", month, days)
+	fmt.Println(sum)
 }
