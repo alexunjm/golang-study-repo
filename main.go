8535de7ba1e86718822e542d615a6e506b1c39ef
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 15:47:37 +0000

    random nums to guess

--- main.go
+++ main.go
@@ -2,43 +2,26 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	// this will split the following string by spaces
-	// and then it will put it inside words variable
-	// as a slice of strings
-	words := strings.Fields(
-		"lazy cat jumps again and again and again",
-	)
-	//    1    2    3    4    5     6    7    8
+	// rand.Seed(10)
+	// rand.Seed(100)
 
-	// --------------------------------
-	// #1st way:
-	// --------------------------------
-	// for j := 0; j < len(words); j++ {
-	// 	fmt.Printf("#%-2d: %q\n", j+1, words[j])
-	// }
+	// t := time.Now()
+	// rand.Seed(t.UnixNano())
 
-	// --------------------------------
-	// #2nd way (best):
-	// --------------------------------
-	for i, v := range words {
-		fmt.Printf("#%-2d: %q\n", i+1, v)
+	// ^-- same:
+
+	rand.Seed(time.Now().UnixNano())
+
+	guess := 10
+
+	for n := 0; n != guess; {
+		n = rand.Intn(guess + 1)
+		fmt.Printf("%d ", n)
 	}
-
-	// --------------------------------
-	// #3rd way (reuse mechanism):
-	// --------------------------------
-	// var (
-	// 	i int
-	// 	v string
-	// )
-
-	// for i, v = range words {
-	// 	fmt.Printf("#%-2d: %q\n", i+1, v)
-	// }
-
-	// fmt.Printf("Last value of i and v %d %q\n", i, v)
+	fmt.Println()
 }
