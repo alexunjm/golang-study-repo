d8615a6b7dae0c80d49abed7f52a3177b95fcb7d
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 14:22:21 +0000

    num of CPUs

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
-import "fmt"
-
-type bot interface {
-	getGreeting() string
-}
-
-type englishBot struct{}
-type spanishBot struct{}
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
-
-	printGreeting(eb)
-	printGreeting(sb)
-}
-
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
-
-func (englishBot) getGreeting() string {
-	return "Hi there!"
-}
-func (spanishBot) getGreeting() string {
-	return "Hola!"
+	fmt.Println(runtime.NumCPU())
 }
