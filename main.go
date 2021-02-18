6aa3a961aee2910ab417168bb5cbd353400d09ab
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 15:45:35 +0000

    Fields split strings by spaces

--- main.go
+++ main.go
@@ -2,46 +2,43 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
 )
 
 func main() {
-	// i := 1
-	// fmt.Printf("%q\n", os.Args[1])
-	// fmt.Printf("%q\n", os.Args[i])
-
-	// i++
-	// fmt.Printf("%q\n", os.Args[2])
-	// fmt.Printf("%q\n", os.Args[i])
-
-	// i++
-	// fmt.Printf("%q\n", os.Args[3])
-	// fmt.Printf("%q\n", os.Args[i])
+	// this will split the following string by spaces
+	// and then it will put it inside words variable
+	// as a slice of strings
+	words := strings.Fields(
+		"lazy cat jumps again and again and again",
+	)
+	//    1    2    3    4    5     6    7    8
 
 	// --------------------------------
 	// #1st way:
 	// --------------------------------
-
-	// for i := 1; i < len(os.Args); i++ {
-	// 	fmt.Printf("%q\n", os.Args[i])
+	// for j := 0; j < len(words); j++ {
+	// 	fmt.Printf("#%-2d: %q\n", j+1, words[j])
 	// }
 
 	// --------------------------------
-	// #2nd way:
+	// #2nd way (best):
 	// --------------------------------
-
-	// for i, v := range os.Args {
-	// 	if i == 0 {
-	// 		continue
-	// 	}
-	// 	fmt.Printf("%q\n", v)
-	// }
-
-	// --------------------------------
-	// #3rd way (best):
-	// --------------------------------
-
-	for _, v := range os.Args[1:] {
-		fmt.Printf("%q\n", v)
+	for i, v := range words {
+		fmt.Printf("#%-2d: %q\n", i+1, v)
 	}
+
+	// --------------------------------
+	// #3rd way (reuse mechanism):
+	// --------------------------------
+	// var (
+	// 	i int
+	// 	v string
+	// )
+
+	// for i, v = range words {
+	// 	fmt.Printf("#%-2d: %q\n", i+1, v)
+	// }
+
+	// fmt.Printf("Last value of i and v %d %q\n", i, v)
 }
