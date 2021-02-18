74cc7b033fe52a39d72d71579edf6ff88da68379
alexanderjaramillo <alexander.jaramillo@the-cocktail.com>
2021-02-18 14:40:06 +0000

    args and string to int parser

--- main.go
+++ main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"strconv"
 )
 
 func main() {
 	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.Version())
+
+	// Itoa doesn't return any errors
+	// So, you don't have to handle the errors for it
+
+	s := strconv.Itoa(42)
+
+	fmt.Println(s)
+
+	// Atoi returns an error value
+	// So, you should always check it
+
+	// go run main.go 39
+	n, err := strconv.Atoi(os.Args[1])
+
+	fmt.Println("Converted number    :", n)
+	fmt.Println("Returned error value:", err)
+
 }
